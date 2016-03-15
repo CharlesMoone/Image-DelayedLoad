33292166fbe2befe9dcfb52e7f22cdd37459fcba
CharlesMoone <meng5994197@gmail.com>
2016-03-15 09:26:01 +0800

    image load
    2015年3月15日更新：
    1、修改了首页的head部分；
    2、对加载做了处理，目前只要用户在需要添加图片的位置添加一个class就可以了；
    3、images必须要。

--- js/imageLoad.js
+++ js/imageLoad.js
@@ -9,14 +9,27 @@
 * imageLoading  : 加载图,这里是关闭其显示
 *
 * */
-function imageLoad(imageSrc, address, imageLoading) {
+function imageLoad(imageSrc) {
+    //图片要防止的位置
+    var address = document.getElementsByClassName("imageLoad")[0];
+
+    //创建一个加载图替代没加载好的图
+    var imageLoading = document.createElement("div");
+    imageLoading.className = "imageLoading";
+    var loadingImage = document.createElement("img");
+    loadingImage.src = "../images/loading.png";
+    loadingImage.alt = "loading";
+    imageLoading.appendChild(loadingImage);
+    address.appendChild(imageLoading);
+
+    //图片加载
     var image = new Image();
     image.src = imageSrc;
     image.onload = function() {
+        console.log("images loaded!");
         //这是为了本地测试,实际使用可移除setTimeout
         setTimeout(function () {
             imageLoading.style.display = "none";
-            console.log("images loaded!");
             address.appendChild(image);
         }, 2000);
     }
